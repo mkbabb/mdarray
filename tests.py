3b24d298dc14773d319618f0a759e46baad980c0
Michael Babb <mike7400@gmail.com>
2019-01-31 21:07:20 -0500

    Clever broadcasting routine implemented, along with a more effective generalised repeat routine (based off of discoveries made during the creation of the broadcasting routine(s))

--- tests.py
+++ tests.py
@@ -198,130 +198,30 @@ End to mdarray and ndarray tests.
 # v = mask(arr, pred)
 # print(v)
 
-def unravel_repeated(arr, raxes, repts):
-    global j
-    mdim = arr.mdim
-    ndim = len(raxes)
-    axis_counter = [0]*mdim
-    arr_out = [0]*arr.size*reduce(lambda x, y: x*y, repts)
 
-    def recurse(ix):
-        global j
-        shape = arr.shape
-        strides = arr.strides
-        axis = shape[ix]
-
-        remaining_axes = mdim - ix
-
-        if remaining_axes == mdim:
-            for i in range(axis):
-                axis_counter[0] = i
-                ix_i = pair_wise_accumulate(axis_counter, strides)
-                arr_out[j] = ix_i
-                j += 1
-        else:
-            for i in range(axis):
-                axis_counter[ix] = i
-                repeated = False
-                for k in range(ndim):
-                    raxis = raxes[k]
-                    rept = repts[k]
-                    if ix == raxis or (raxis == 0 and ix == 1):
-                        for l in range(rept):
-                            recurse(ix - 1)
-                        repeated = True
-                        break
-                if not repeated:
-                    recurse(ix - 1)
-    j = 0
-    recurse(mdim - 1)
-    return arr_out
-
-
-def broadcast_copy(arr1, arr2):
-    mdim1 = arr1.mdim
-    mdim2 = arr2.mdim
-    shape1 = arr1.shape
-    shape2 = arr2.shape
-
-    mdim = mdim1
-
-    if mdim1 > mdim2:
-        arr2.reshape(shape2 + [1]*(mdim1 - mdim2))
-    elif mdim1 < mdim2:
-        arr1.reshape(shape1 + [1]*(mdim2 - mdim1))
-        mdim = mdim2
-
-    shape1 = arr1.shape
-    shape2 = arr2.shape
-
-    for i in range(mdim):
-        axis1_i = shape1[i]
-        axis2_i = shape2[i]
-        if axis1_i == 1 and axis2_i > 1:
-            arr1 = repeat(arr1, i, axis2_i)
-        elif axis1_i > 1 and axis2_i == 1:
-            arr2 = repeat(arr2, i, axis1_i)
-        elif axis1_i != axis2_i:
-            raise IncompatibleDimensions
-        # print(arr1)
-
-        # print('\n\n\n')
-    return arr1, arr2
 
 
 # t0 = expand_dims(myslice, arr)
-shape = [1, 2, 3, 4]
+shape = [4, 3, 2]
 size = reduce(lambda x, y: x*y, shape)
-xx = arange(size).reshape(shape)
-y = arange(2).reshape([2, 1, 1, 1]) + 2
+X = arange(size).reshape(shape)
+print(X)
+
+v = repeat(X, [2], [1])
+print(v)
+print(v.shape)
+
+# Y = ones(shape=[1])*3
 
 
-raxes = [0, 1, 2]
-repts = [2, 3, 1]
+# def f(x, y): return (x + y)
 
 
-def super_repeat(arr, raxes, repts):
-    def recurse(ix):
-        if ix == 0:
-            # print(raxes[0], repts[0])
-            unravel_repeated(arr, raxes[0], 6)
-        else:
-            for i in range(repts[ix]):
-                print(ix)
-                recurse(ix - 1)
-    recurse(len(raxes) - 1)
-
-
-# super_repeat(xx, raxes, repts)
-# print(y)
-# unravel_repeated(xx, [0, 2], [2, 4])
-ix1 = unravel_repeated(y, [1, 2, 3], [2, 3, 4])
-ix2 = unravel_repeated(xx, [0], [2])
-print(len(ix1), len(ix2))
-print(ix1)
-print(ix2)
-
-
-# v = repeat(y, 3, 5)
-# print(xx)
-# print(v.shape)
-
-# print(xx)
-# print('\n')
-# print(y)
-# print('\n')
-
-xx, y = broadcast_copy(xx, y)
-print(xx)
-# print(y)
-# v = xx + y
+# v = broadcast(X, Y, f)
 # print(v)
-
 # print(v.shape)
 
-# print(xx + y)
-
-# unravel_repeated(xx, 0, 4)
-
-# xx, y = broadcast_copy(xx, y)
+# X = md.tondarray(X)
+# y = md.tondarray(Y)
+# v = (X + Y)
+# print(v)
