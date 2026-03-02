a4026639673c43fdeb9694d3e7962d6bcab4983b
Mike Babb <mike7400@gmail.com>
2026-03-01 20:08:08 -0500

    Remove old source files and legacy tests
    Delete root-level modules (MultiArray.py, marray.py, toast.py, tmp.py),
    old core/, fft/, linalg/, iterators/, misc/ directories, deprecated
    code, and legacy test files. All functionality has been consolidated
    into src/mdarray/.

--- core/depreciated/__init__.py
+++
@@ -1,3 +0,0 @@
-from core.depreciated.creation import *
-from core.depreciated.manipulation import *
-from core.depreciated.formatting import *
--- core/exceptions.py
+++
@@ -1,2 +0,0 @@
-class IncompatibleDimensions(Exception):
-    pass
